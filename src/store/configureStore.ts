7fa9fe595aafe4ca68de6b70ade70033437415f7
mavisjheng <erjhen@gmail.com>
2017-10-11 11:01:02 +0800

    add redux-logger when dev

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -6,11 +6,16 @@ import rootReducer from '../reducers';
 // Create a history of your choosing (we're using a browser history in this case)
 export const history = createHistory();
 // Build the middleware for intercepting and dispatching navigation actions
-const middleware = routerMiddleware(history);
+const middlewares = [routerMiddleware(history)];
+
+if (process.env.NODE_ENV === `development`) {
+  const { logger } = require(`redux-logger`);
+  middlewares.push(logger);
+}
 
 export function configureStore() {
   return createStore(
     rootReducer,
-    applyMiddleware(middleware)
+    applyMiddleware(...middlewares)
   );
 }
